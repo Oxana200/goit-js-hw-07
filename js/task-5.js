a8e5adb0f5af51d3edbb698229ec1b892f59d50b
oxana <oxanamoisiuk7@gmail.com>
2025-01-18 15:58:15 +0200

    all

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.body;
+
+changeColorButton.addEventListener('click', () => {
+  const newColor = getRandomHexColor(); // Генеруємо новий випадковий колір
+  body.style.backgroundColor = newColor; // Задаємо фон <body>
+  colorSpan.textContent = newColor; // Вставляємо колір у span.color
+});
+
